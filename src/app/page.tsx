19058c7f170d710c58b95c4102d9e9069f9e6e1e
send4t <133424813+send4t@users.noreply.github.com>
2024-11-18 12:32:58 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,9 @@ export default async function Home() {
       <Header />
       <main>
         <Suspense fallback={<div>Loading...</div>}>
-         <PrimaryFeatures /> important for main page
+         <PrimaryFeatures /> 
             <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none pt-10">
               <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl text-center mt">
-              Fejlesztés alatt/Under construction
               </h2>
             </div>
           
